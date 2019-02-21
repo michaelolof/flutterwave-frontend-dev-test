c36e671930b4a0e6a3f9898782fd5b32112f69e3
michaelolof <myqrrlolof@yahoo.com>
2019-02-21 10:48:15 +0100

    Refractoring to mobile first design

--- src/components/ImageRow.vue
+++ src/components/ImageRow.vue
@@ -2,10 +2,16 @@
   <div class="image-row">
     <div v-for="image in row" :key="image" class="image">
       <img :src="'/assets/' + image" />
+      <div class="overlay">
+        <div class="image-details">
+          <div class="price">N120,000</div>
+          <div class="availability">Available in size 44, 45, 46</div>
+          <button class="buy-now">Buy Now</button>
+        </div>
+      </div>
     </div>
   </div>  
 </template>
-
 
 <script>
   export default {
